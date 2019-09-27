8aca858881528feea6267469e52fec0f8d68f954
Alio Agudo <alio.agudo@soprasteria.com>
2019-09-27 11:44:56 +0200

    feat: fake mocked data created

---
+++ api/endpoints/lineup-players.js
@@ -0,0 +1,21 @@
+export const lineupPlayers = {
+    porteros: [
+        {name: 'Iker Casillas', price: '5.000.000', points: '20', team: 'Real Madrid', position: 'PT', id:1}
+    ],
+    defensores: [
+        {name: 'Sergio Ramos', price: '6.000.000', points: '15', team: 'Real Madrid', position: 'DF', id: 2},
+        {name: 'Roberto Carlos', price: '5.500.000', points: '20', team: 'Real Madrid', position: 'DF', id: 3},
+        {name: 'Gerard Piqué', price: '5.000.000', points: '10', team: 'Barcelona', position: 'DF', id: 4},
+        {name: 'Dani Carvajal', price: '4.000.000', points: '35', team: 'Real Madrid', position: 'DF', id:5}
+    ],
+    medios: [
+        {name: 'Iniesta', price: '8.000.000', points: '20', team: 'Barcelona', position: 'MC', id: 6},
+        {name: 'Luka Modric', price: '7.000.000', points: '40', team: 'Real Madrid', position: 'MC', id: 7},
+        {name: 'Xavi', price: '4.000.000', points: '40', team: 'Barcelona', position: 'MC', id: 8},
+    ],
+    delanteros: [
+        {name: 'Lionel Messi', price: '9.000.000', points: '40', team: 'Barcelona', position: 'DL', id: 9},
+        {name: 'Cristiano Ronaldo', price: '9.000.000', points: '45', team: 'Real Madrid', position: 'DL', id: 10},
+        {name: 'Ronaldinho', price: '8.500.000', points: '30', team: 'Barcelona', position: 'DL', id: 11}
+    ]  
+};
---
+++ api/endpoints/team-players.js
@@ -0,0 +1,26 @@
+export const teamPlayers = {
+    porteros: [
+        {name: 'Iker Casillas', price: '5.000.000', points: '20', team: 'Real Madrid', position: 'PT'}
+    ],
+    defensores: [
+        {name: 'Sergio Ramos', price: '6.000.000', points: '15', team: 'Real Madrid', position: 'DF'},
+        {name: 'Roberto Carlos', price: '5.500.000', points: '20', team: 'Real Madrid', position: 'DF'},
+        {name: 'Gerard Piqué', price: '5.000.000', points: '10', team: 'Barcelona', position: 'DF'},
+        {name: 'Dani Carvajal', price: '4.000.000', points: '35', team: 'Real Madrid', position: 'DF'},
+        {name: 'Marcelo', price: '3.500.000', points: '35', team: 'Real Madrid', position: 'DF'}
+    ],
+    medios: [
+        {name: 'Iniesta', price: '8.000.000', points: '20', team: 'Barcelona', position: 'MC'},
+        {name: 'Luka Modric', price: '7.000.000', points: '40', team: 'Real Madrid', position: 'MC'},
+        {name: 'Xavi', price: '4.000.000', points: '40', team: 'Barcelona', position: 'MC'},
+        {name: 'PEPITO PEREZ', price: '8.000.000', points: '20', team: 'MI CASA', position: 'MC'},
+        {name: 'Casemiro', price: '5.000.000', points: '40', team: 'Rea Madrid', position: 'MC'},
+
+    ],
+    delanteros: [
+        {name: 'Lionel Messi', price: '9.000.000', points: '40', team: 'Barcelona', position: 'DL'},
+        {name: 'Cristiano Ronaldo', price: '9.000.000', points: '45', team: 'Real Madrid', position: 'DL'},
+        {name: 'Ronaldinho', price: '8.500.000', points: '30', team: 'Barcelona', position: 'DL'},
+        {name: 'Neymar', price: '6.500.000', points: '50', team: 'PSG', position: 'DL'}
+    ]  
+};
